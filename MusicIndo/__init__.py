8d3025abe8d50a11c67956487301bc270c6a226f
bendotz <opahzahra9@gmail.com>
2025-12-01 23:17:04 +0700

    Update __init__.py

--- MusicIndo/__init__.py
+++ MusicIndo/__init__.py
@@ -1,9 +1,5 @@
 #
-import asyncio as _asyncio
-
-import uvloop as _uvloop
-
-_asyncio.set_event_loop_policy(_uvloop.EventLoopPolicy())  # noqa
+import asyncio
 
 from MusicIndo.core.bot import YukkiBot
 from MusicIndo.core.dir import dirr
