02ae550908da989aca7ae1755b3cb6eb4cb251cc
bendotz <opahzahra9@gmail.com>
2025-12-01 23:21:11 +0700

    Update bot.py

--- MusicIndo/core/bot.py
+++ MusicIndo/core/bot.py
@@ -1,23 +1,13 @@
-#
-#
 import asyncio
 import importlib.util
 import os
 import traceback
 from datetime import datetime
 from functools import wraps
+from functools import wraps
 
-from pyrogram import Client, StopPropagation, errors
+from pyrogram import Client, StopPropagation
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import (
-    ChatSendMediaForbidden,
-    ChatSendPhotosForbidden,
-    ChatWriteForbidden,
-    FloodWait,
-    MessageIdInvalid,
-    MessageNotModified,
-)
-from pyrogram.handlers import MessageHandler
 from pyrogram.types import (
     BotCommand,
     BotCommandScopeAllChatAdministrators,
@@ -27,6 +17,11 @@ from pyrogram.types import (
     BotCommandScopeChatMember,
 )
 
+from pyrogram.errors import RPCError
+from pyrogram.errors.exceptions.forbidden_403 import Forbidden
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.errors.exceptions.bad_request_400 import BadRequest
+from pyrogram.errors.exceptions.bad_request_400 import MessageIdInvalid, MessageNotModified
 import config
 
 from ..logging import LOGGER
@@ -52,22 +47,13 @@ class YukkiBot(Client):
             @wraps(func)
             async def wrapper(client, message):
                 try:
-                    if asyncio.iscoroutinefunction(func):
-                        await func(client, message)
-                    else:
-                        func(client, message)
+                    await func(client, message)
                 except FloodWait as e:
                     LOGGER(__name__).warning(
                         f"FloodWait: Sleeping for {e.value} seconds."
                     )
                     await asyncio.sleep(e.value)
-                except (
-                    ChatWriteForbidden,
-                    ChatSendMediaForbidden,
-                    ChatSendPhotosForbidden,
-                    MessageNotModified,
-                    MessageIdInvalid,
-                ):
+                except (RPCError, Forbidden, BadRequest, MessageNotModified, MessageIdInvalid):
                     pass
                 except StopPropagation:
                     raise
@@ -88,8 +74,8 @@ class YukkiBot(Client):
                         f"<b>Chat ID:</b> {chat_id}\n"
                         f"<b>Chat Username:</b> {chat_username}\n"
                         f"<b>User ID:</b> {user_id}\n"
-                        f"<b>Command/Text:</b>\n<pre language='python'><code>{command}</code></pre>\n\n"
-                        f"<b>Traceback:</b>\n<pre language='python'><code>{error_trace}</code></pre>"
+                        f"<b>Command/Text:</b>\n<pre><code>{command}</code></pre>\n\n"
+                        f"<b>Traceback:</b>\n<pre><code>{error_trace}</code></pre>"
                     )
                     await self.send_message(config.LOG_GROUP_ID, error_message)
                     try:
@@ -121,152 +107,11 @@ class YukkiBot(Client):
                     f"Username : @{self.username}"
                 ),
             )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot failed to access the log group. Ensure the bot is added and promoted as admin."
-            )
-            LOGGER(__name__).error("Error details:", exc_info=True)
-            exit()
-        if config.SET_CMDS:
-            try:
-                await self._set_default_commands()
-            except Exception:
-                LOGGER(__name__).warning("Failed to set commands:", exc_info=True)
-
-        try:
-            a = await self.get_chat_member(config.LOG_GROUP_ID, "me")
-            if a.status != ChatMemberStatus.ADMINISTRATOR:
-                LOGGER(__name__).error("Please promote bot as admin in logger group")
-                exit()
         except Exception:
-            pass
-        LOGGER(__name__).info(f"MusicBot started as {self.name}")
-
-    async def _set_default_commands(self):
-        private_commands = [
-            BotCommand("start", "Start the bot"),
-            BotCommand("help", "Get the help menu"),
-            BotCommand("ping", "Check if the bot is alive or dead"),
-        ]
-        group_commands = [BotCommand("play", "Start playing requested song")]
-        admin_commands = [
-            BotCommand("play", "Start playing requested song"),
-            BotCommand("skip", "Move to next track in queue"),
-            BotCommand("pause", "Pause the current playing song"),
-            BotCommand("resume", "Resume the paused song"),
-            BotCommand("end", "Clear the queue and leave voice chat"),
-            BotCommand("shuffle", "Randomly shuffle the queued playlist"),
-            BotCommand("playmode", "Change the default playmode for your chat"),
-            BotCommand("settings", "Open bot settings for your chat"),
-        ]
-        owner_commands = [
-            BotCommand("update", "Update the bot"),
-            BotCommand("restart", "Restart the bot"),
-            BotCommand("logs", "Get logs"),
-            BotCommand("export", "Export all data of mongodb"),
-            BotCommand("import", "Import all data in mongodb"),
-            BotCommand("addsudo", "Add a user as a sudoer"),
-            BotCommand("delsudo", "Remove a user from sudoers"),
-            BotCommand("sudolist", "List all sudo users"),
-            BotCommand("log", "Get the bot logs"),
-            BotCommand("getvar", "Get a specific environment variable"),
-            BotCommand("delvar", "Delete a specific environment variable"),
-            BotCommand("setvar", "Set a specific environment variable"),
-            BotCommand("usage", "Get dyno usage information"),
-            BotCommand("maintenance", "Enable or disable maintenance mode"),
-            BotCommand("logger", "Enable or disable logging"),
-            BotCommand("block", "Block a user"),
-            BotCommand("unblock", "Unblock a user"),
-            BotCommand("blacklist", "Blacklist a chat"),
-            BotCommand("whitelist", "Whitelist a chat"),
-            BotCommand("blacklisted", "List all blacklisted chats"),
-            BotCommand("autoend", "Enable or disable auto end for streams"),
-            BotCommand("reboot", "Reboot the bot"),
-            BotCommand("restart", "Restart the bot"),
-        ]
-
-        await self.set_bot_commands(
-            private_commands, scope=BotCommandScopeAllPrivateChats()
-        )
-        await self.set_bot_commands(
-            group_commands, scope=BotCommandScopeAllGroupChats()
-        )
-        await self.set_bot_commands(
-            admin_commands, scope=BotCommandScopeAllChatAdministrators()
-        )
-
-        LOG_GROUP_ID = (
-            f"@{config.LOG_GROUP_ID}"
-            if isinstance(config.LOG_GROUP_ID, str)
-            and not config.LOG_GROUP_ID.startswith("@")
-            else config.LOG_GROUP_ID
-        )
-
-        for owner_id in config.OWNER_ID:
-            try:
-                await self.set_bot_commands(
-                    owner_commands,
-                    scope=BotCommandScopeChatMember(
-                        chat_id=LOG_GROUP_ID, user_id=owner_id
-                    ),
-                )
-                await self.set_bot_commands(
-                    private_commands + owner_commands,
-                    scope=BotCommandScopeChat(chat_id=owner_id),
-                )
-            except Exception:
-                pass
-
-    def load_plugin(self, file_path: str, base_dir: str, utils=None):
-        file_name = os.path.basename(file_path)
-        module_name, ext = os.path.splitext(file_name)
-        if module_name.startswith("__") or ext != ".py":
-            return None
-
-        relative_path = os.path.relpath(file_path, base_dir).replace(os.sep, ".")
-        module_path = f"{os.path.basename(base_dir)}.{relative_path[:-3]}"
-
-        spec = importlib.util.spec_from_file_location(module_path, file_path)
-        module = importlib.util.module_from_spec(spec)
-        module.logger = LOGGER(module_path)
-        module.app = self
-        module.Config = config
-
-        if utils:
-            module.utils = utils
-
-        try:
-            spec.loader.exec_module(module)
-            self.loaded_plug_counts += 1
-        except Exception as e:
-            LOGGER(__name__).error(
-                f"Failed to load {module_path}: {e}\n\n", exc_info=True
-            )
+            LOGGER(__name__).error("Bot failed to access the log group.", exc_info=True)
             exit()
 
-        return module
-
-    def load_plugins_from(self, base_folder: str):
-        base_dir = os.path.abspath(base_folder)
-        utils_path = os.path.join(base_dir, "utils.py")
-        utils = None
-
-        if os.path.exists(utils_path) and os.path.isfile(utils_path):
-            try:
-                spec = importlib.util.spec_from_file_location("utils", utils_path)
-                utils = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(utils)
-            except Exception as e:
-                LOGGER(__name__).error(
-                    f"Failed to load 'utils' module: {e}", exc_info=True
-                )
-
-        for root, _, files in os.walk(base_dir):
-            for file in files:
-                if file.endswith(".py") and not file == "utils.py":
-                    file_path = os.path.join(root, file)
-                    mod = self.load_plugin(file_path, base_dir, utils)
-                    yield mod
+        LOGGER(__name__).info(f"MusicBot started as {self.name}")
 
     async def run_shell_command(self, command: list):
         process = await asyncio.create_subprocess_exec(
@@ -282,3 +127,9 @@ class YukkiBot(Client):
             "stdout": stdout.decode().strip() if stdout else None,
             "stderr": stderr.decode().strip() if stderr else None,
         }
+
+
+# ====== INITIALIZER BOT INSTANCE ======
+app = YukkiBot()
+userbot = Userbot()
+HELPABLE = {}
